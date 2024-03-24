ad253beaaa0d9281c53a8771ac1930424dd63a29
Andy <5783068+ZephrFish@users.noreply.github.com>
2024-03-24 01:03:23 +0000

    Added updated messaging to prepare for X number of chunks then confirmation at the end of amount copied including 0 as starting number

--- ChunkyIngress.ps1
+++ ChunkyIngress.ps1
@@ -6,14 +6,17 @@ function ChunkyIngress {
     )
 
     $base64String = [Convert]::ToBase64String([IO.File]::ReadAllBytes($filePath))
-    $chunkSize = 68KB  # Change this to whatever limit you want, in my case 68kb was the limit
+    $chunkSize = 68KB  # Change this to whatever limit you want; in my case, 68kb was the limit
     $chunkCount = [math]::Ceiling($base64String.Length / $chunkSize)
+       
+    Write-Output "Prepare for ${chunkCount} blocks of data"
+    Write-Output "Once you have pasted each into your env, hit return to copy the next block"
 
     for ($i = 0; $i -lt $chunkCount; $i++) {
         $startIndex = $i * $chunkSize
         $chunk = $base64String.Substring($startIndex, [math]::Min($chunkSize, $base64String.Length - $startIndex))
         Set-Clipboard -Value $chunk
-        Write-Output "Chunk $i copied to clipboard. Press Enter to copy next chunk or Ctrl+C to exit."
+        Write-Output "Chunk $i copied to clipboard. Press Enter to copy the next chunk or Ctrl+C to exit."
         Read-Host
     }
 
